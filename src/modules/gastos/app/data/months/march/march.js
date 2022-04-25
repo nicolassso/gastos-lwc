8e3b057fed4cf4677f200e572af148a43b1554d5
Nicolás Leal <49799875+nicolassso@users.noreply.github.com>
2022-04-25 22:13:33 +0200

    Moved data folder

---
+++ src/modules/gastos/app/data/months/march/march.js
@@ -0,0 +1,13 @@
+export const MARCH_DATA = {
+        march: {
+            rent: 460,
+            debt: 150,
+            food: 231.95,
+            travel: 101.72,
+            cash: 20,
+            gym: 33,
+            sex: 12.9,
+            beauty: 10
+        },
+        total: 1019.57
+}
