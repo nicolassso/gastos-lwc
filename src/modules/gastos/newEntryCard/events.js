693c2c739349788403b1ca12be3469396a12d46a
Nicolás Leal <49799875+nicolassso@users.noreply.github.com>
2022-04-25 23:11:10 +0200

    New SubmitEntry event

---
+++ src/modules/gastos/newEntryCard/events.js
@@ -0,0 +1,9 @@
+export class SubmitEntry extends CustomEvent {
+    constructor(detail) {
+        super(SubmitEntry.type, {detail});
+    }
+
+    static get type() {
+        return 'submitentry';
+    }
+}
--- src/modules/gastos/newEntryCard/newEntryCard.js
+++ src/modules/gastos/newEntryCard/newEntryCard.js
@@ -1,4 +1,24 @@
 import { LightningElement } from 'lwc';
+import { SubmitEntry } from './events';
 
 export default class NerCardEntry extends LightningElement {
+
+    categoryInput = '';
+    keyInput = '';
+    quantityInput = 0;
+
+    handleInputChange(event) {
+        this[event.path[0].id] = event.target.value
+    }
+
+
+    handleButtonClick() {
+        const newEntry = {
+            category: this.categoryInput,
+            key: this.keyInput,
+            quantity: parseFloat(this.quantityInput)
+        }
+        this.dispatchEvent(new SubmitEntry(newEntry))
+    }
+
 }
