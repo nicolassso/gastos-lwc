8a0b7a1ce029007872006e0546c82ea35b417f8e
Nicolás Leal <49799875+nicolassso@users.noreply.github.com>
2022-04-27 00:28:06 +0200

    Add getters logic

--- src/modules/gastos/app/app.js
+++ src/modules/gastos/app/app.js
@@ -1,15 +1,35 @@
 import { LightningElement } from 'lwc';
 import { currentMonth } from './constants.js';
 import { APRIL_DATA } from './data/months/april/april';
-import { MARCH_DATA } from './data/months/march/march';
+import {APRIL_ENTRIES} from './data/months/april/entries';
+
+import { entriesToObject, totalExpensesByCategory, totalExpenses } from './main/utils.js';
 
 
 export default class App extends LightningElement {
-    month = currentMonth
-    data = APRIL_DATA
-    newEntry = {}
 
-    handleSubmitEntry(event){
-        this.newEntry = event.detail
+    month = currentMonth;
+    data = APRIL_DATA;
+    entries = APRIL_ENTRIES;
+    newEntry = {};
+
+
+    handleSubmitEntry({detail}){
+        this.newEntry = JSON.parse(JSON.stringify(detail))
+        this.entries.push(this.newEntry)
+        this.data.april = this.totalExpensesByCategory
+        this.data.total = this.totalExpenses
+    }
+
+    get totalExpenses() {
+        return totalExpenses(this.totalExpensesByCategory)
+    }
+
+    get totalExpensesByCategory() {
+        return totalExpensesByCategory(this.entriesToObject)
+    }
+
+    get entriesToObject() {
+        return entriesToObject(this.entries)
     }
 }
