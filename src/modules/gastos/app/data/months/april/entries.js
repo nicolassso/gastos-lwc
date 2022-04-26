6daaa07cc18a2e775d612e13fe5d2336b04da790
Nicolás Leal <49799875+nicolassso@users.noreply.github.com>
2022-04-27 00:25:56 +0200

    Change json file formats to js

---
+++ src/modules/gastos/app/data/months/april/entries.js
@@ -0,0 +1,102 @@
+export const APRIL_ENTRIES = [
+    {
+        "category": "rent",
+        "key": "rent",
+        "quantity": [360]
+    },
+    {
+        "category": "rent",
+        "key": "bail",
+        "quantity": [360]
+    },
+    {
+        "category": "debt",
+        "key": "paid",
+        "quantity": [20]
+    },
+    {
+        "category": "food",
+        "key": "restaurant",
+        "quantity": [
+            3.2, 14.1, 4.2, 17.55, 7.5, 7.7, 8, 13.6, 8.9, 11, 4.95, 6,
+            5.6, 20.8, 19.5, -36.6, 21.9
+        ]
+    },
+    {
+        "category": "food",
+        "key": "market",
+        "quantity": [
+            1.15, 31.05, 3.2, 2.64, 8.63, 5.8, 24.36, 23.99, 4.34
+        ]
+    },
+    {
+        "category": "food",
+        "key": "coffee",
+        "quantity": [2.8, 6.1, 8.1]
+    },
+    {
+        "category": "travel",
+        "key": "metro",
+        "quantity": []
+    },
+    {
+        "category": "travel",
+        "key": "bus",
+        "quantity": [10, 31.62, 10]
+    },
+    {
+        "category": "travel",
+        "key": "gas",
+        "quantity": [10]
+    },
+    {
+        "category": "cash",
+        "key": null,
+        "quantity": [10, 6.2, 20, -5]
+    },
+    {
+        "category": "gym",
+        "key": null,
+        "quantity": []
+    },
+    {
+        "category": "other",
+        "key": "presents",
+        "quantity": [24, 6.35, 26.57]
+    },
+    {
+        "category": "other",
+        "key": "pharmacy",
+        "quantity": [4.89, 11.95]
+    },
+    {
+        "category": "other",
+        "key": "spotify",
+        "quantity": [9.99]
+    },
+    {
+        "category": "home",
+        "key": "wifi",
+        "quantity": [11.15]
+    },
+    {
+        "category": "vice",
+        "key": "alcohol",
+        "quantity": [8.6, 8.6, 8.6, 4.4, 4.3, 15, 10, 14]
+    },
+    {
+        "category": "vice",
+        "key": "weed",
+        "quantity": [20, 5]
+    },
+    {
+        "category": "travel",
+        "key": "scooter",
+        "quantity": [2.85]
+    },
+    {
+        "category": "home",
+        "key": "coffee",
+        "quantity": [10.65]
+    }
+]
