e81a937dd69491e16bd77fa8e561a31627c923ae
Nicolás Leal <49799875+nicolassso@users.noreply.github.com>
2022-04-30 22:19:31 +0200

    add may folder

---
+++ src/modules/gastos/app/data/months/may/entries.js
@@ -0,0 +1,97 @@
+export const MARCH_ENTRIES = [
+    {
+        category: 'rent',
+        key: 'rent',
+        quantity: [360],
+    },
+    {
+        category: 'rent',
+        key: 'bail',
+        quantity: [],
+    },
+    {
+        category: 'debt',
+        key: 'paid',
+        quantity: [150],
+    },
+    {
+        category: 'food',
+        key: 'restaurant',
+        quantity: [9.3, 6.6, 39.4, 26.9],
+    },
+    {
+        category: 'food',
+        key: 'market',
+        quantity: [1.8, 16.33],
+    },
+    {
+        category: 'food',
+        key: 'coffee',
+        quantity: [2.6],
+    },
+    {
+        category: 'travel',
+        key: 'metro',
+        quantity: [],
+    },
+    {
+        category: 'travel',
+        key: 'bus',
+        quantity: [16.11],
+    },
+    {
+        category: 'travel',
+        key: 'gas',
+        quantity: [10],
+    },
+    {
+        category: 'cash',
+        key: null,
+        quantity: [20],
+    },
+    {
+        category: 'gym',
+        key: null,
+        quantity: [73],
+    },
+    {
+        category: 'other',
+        key: 'presents',
+        quantity: [],
+    },
+    {
+        category: 'other',
+        key: 'pharmacy',
+        quantity: [],
+    },
+    {
+        category: 'other',
+        key: 'spotify',
+        quantity: [],
+    },
+    {
+        category: 'home',
+        key: 'wifi',
+        quantity: [11.15],
+    },
+    {
+        category: 'vice',
+        key: 'alcohol',
+        quantity: [],
+    },
+    {
+        category: 'vice',
+        key: 'weed',
+        quantity: [],
+    },
+    {
+        category: 'other',
+        key: 'clothes',
+        quantity: [50],
+    },
+    {
+        category: 'other',
+        key: 'book',
+        quantity: [7],
+    },
+];
