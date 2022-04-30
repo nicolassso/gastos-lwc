ced89a5f6877abc74451b378136ecf4ca2247b27
Nicolás Leal <49799875+nicolassso@users.noreply.github.com>
2022-04-30 22:19:14 +0200

    apply prettier

--- src/modules/gastos/app/app.js
+++ src/modules/gastos/app/app.js
@@ -1,35 +1,36 @@
 import { LightningElement } from 'lwc';
 import { currentMonth } from './constants.js';
 import { APRIL_DATA } from './data/months/april/april';
-import {APRIL_ENTRIES} from './data/months/april/entries';
+import { APRIL_ENTRIES } from './data/months/april/entries';
 
-import { entriesToObject, totalExpensesByCategory, totalExpenses } from './main/utils.js';
-
+import {
+    entriesToObject,
+    totalExpensesByCategory,
+    totalExpenses,
+} from './main/utils.js';
 
 export default class App extends LightningElement {
-
     month = currentMonth;
     data = APRIL_DATA;
     entries = APRIL_ENTRIES;
     newEntry = {};
 
-
-    handleSubmitEntry({detail}){
-        this.newEntry = JSON.parse(JSON.stringify(detail))
-        this.entries.push(this.newEntry)
-        this.data.april = this.totalExpensesByCategory
-        this.data.total = this.totalExpenses
+    handleSubmitEntry({ detail }) {
+        this.newEntry = JSON.parse(JSON.stringify(detail));
+        this.entries.push(this.newEntry);
+        this.data.april = this.totalExpensesByCategory;
+        this.data.total = this.totalExpenses;
     }
 
     get totalExpenses() {
-        return totalExpenses(this.totalExpensesByCategory)
+        return totalExpenses(this.totalExpensesByCategory);
     }
 
     get totalExpensesByCategory() {
-        return totalExpensesByCategory(this.entriesToObject)
+        return totalExpensesByCategory(this.entriesToObject);
     }
 
     get entriesToObject() {
-        return entriesToObject(this.entries)
+        return entriesToObject(this.entries);
     }
 }
--- src/modules/gastos/newEntryCard/newEntryCard.js
+++ src/modules/gastos/newEntryCard/newEntryCard.js
@@ -2,23 +2,20 @@ import { LightningElement } from 'lwc';
 import { SubmitEntry } from './events';
 
 export default class NewCardEntry extends LightningElement {
-
     categoryInput = 'test';
     keyInput = 'rent';
     quantityInput = [135];
 
     handleInputChange(event) {
-        this[event.path[0].id] = event.target.value
+        this[event.path[0].id] = event.target.value;
     }
-
 
     handleButtonClick() {
         const newEntry = {
             category: this.categoryInput,
             key: this.keyInput,
-            quantity: [parseFloat(this.quantityInput)]
-        }
-        this.dispatchEvent(new SubmitEntry(newEntry))
+            quantity: [parseFloat(this.quantityInput)],
+        };
+        this.dispatchEvent(new SubmitEntry(newEntry));
     }
-
 }
