b21e822cc91a3133a2c5476f35b2a8adca168b7f
Nicolás Leal <49799875+nicolassso@users.noreply.github.com>
2022-05-04 22:23:29 +0200

    Make new entry key input a picklist depending on the category input value

--- src/modules/gastos/newEntryCard/newEntryCard.js
+++ src/modules/gastos/newEntryCard/newEntryCard.js
@@ -1,11 +1,10 @@
 import { LightningElement } from 'lwc';
 import { SubmitEntry } from './events';
-import { CATEGORIES } from './constants';
 import { CATEGORIES, KEYS } from './constants';
 export default class NewCardEntry extends LightningElement {
-  categoryInput = 'rent';
-  keyInput = 'rent';
-  quantityInput = [0];
+  categoryInput;
+  keyInput;
+  quantityInput;
 
   handleInputChange(event) {
     this[event.path[0].id] = event.target.value;
@@ -26,5 +25,8 @@ export default class NewCardEntry extends LightningElement {
 
   get id() {
     return this.categories.map((c) => `${c}-select`);
+  }
+  get selectedKey() {
+    return KEYS[this.categoryInput];
   }
 }
